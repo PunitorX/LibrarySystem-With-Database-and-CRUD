a913f1510c2d1bb8c158ee2af67eca1c842e6316
Robert Jones <jones.brendan1996@gmail.com>
2022-06-06 21:04:15 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace LibrarySystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBook = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboGenre = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtPublisher = new System.Windows.Forms.TextBox();
+            this.txtRelease = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name ";
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(288, 87);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(127, 22);
+            this.txtFirst.TabIndex = 1;
+            // 
+            // txtLast
+            // 
+            this.txtLast.Location = new System.Drawing.Point(288, 133);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(127, 22);
+            this.txtLast.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(154, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Name ";
+            // 
+            // txtBook
+            // 
+            this.txtBook.Location = new System.Drawing.Point(288, 39);
+            this.txtBook.Name = "txtBook";
+            this.txtBook.Size = new System.Drawing.Size(127, 22);
+            this.txtBook.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(154, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Title";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(436, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Genre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(436, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Release Year";
+            // 
+            // comboGenre
+            // 
+            this.comboGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboGenre.FormattingEnabled = true;
+            this.comboGenre.Location = new System.Drawing.Point(570, 84);
+            this.comboGenre.Name = "comboGenre";
+            this.comboGenre.Size = new System.Drawing.Size(127, 30);
+            this.comboGenre.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(436, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Publisher";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(107, 196);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(123, 66);
+            this.btnAdd.TabIndex = 19;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(236, 196);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(123, 66);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(365, 196);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(123, 66);
+            this.btnUpdate.TabIndex = 21;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(494, 196);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(123, 66);
+            this.btnClear.TabIndex = 22;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 295);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(770, 171);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(623, 196);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(123, 66);
+            this.btnLoad.TabIndex = 25;
+            this.btnLoad.Text = "Load All";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // txtPublisher
+            // 
+            this.txtPublisher.Location = new System.Drawing.Point(570, 39);
+            this.txtPublisher.Name = "txtPublisher";
+            this.txtPublisher.Size = new System.Drawing.Size(127, 22);
+            this.txtPublisher.TabIndex = 28;
+            // 
+            // txtRelease
+            // 
+            this.txtRelease.Location = new System.Drawing.Point(570, 134);
+            this.txtRelease.Name = "txtRelease";
+            this.txtRelease.Size = new System.Drawing.Size(127, 22);
+            this.txtRelease.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 510);
+            this.Controls.Add(this.txtRelease);
+            this.Controls.Add(this.txtPublisher);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboGenre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBook);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLast);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Library System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBook;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboGenre;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox txtPublisher;
+        private System.Windows.Forms.TextBox txtRelease;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LibrarySystem
+{
+    public partial class Form1 : Form
+    {
+        //The connection string to connect the winForm application to the Database 'LibraryBooks' and then the table 'Books'.
+        //Database is on Microsoft SQL Server
+        string connectionString = @"Data Source=PUNITOR\SQLEXPRESS;Initial Catalog=LibraryBooks;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Adds strings to be selected on the 'comboGenre' combobox
+            comboGenre.Text = "Please Select An Item";
+            comboGenre.Items.Add("Comedy");
+            comboGenre.Items.Add("Horror");
+            comboGenre.Items.Add("Fiction");
+            comboGenre.Items.Add("Fantasy");
+            comboGenre.Items.Add("Mystery");
+            comboGenre.Items.Add("Thriller");
+            comboGenre.Items.Add("Historical");
+            comboGenre.Items.Add("Romance");
+            comboGenre.Items.Add("Western");
+            comboGenre.Items.Add("Sci-Fi");
+            comboGenre.Items.Add("Dystopian");
+        }
+        
+        //Loads the data from the database.
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                //...
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM Books", con);
+                DataTable dt = new DataTable();
+                sqlDa.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+            }
+        }
+        
+        //Adds data
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Books values(@Book, @First_Name, @Last_Name, @Publisher, @Genre, @Release_Year)", con);
+                cmd.Parameters.AddWithValue("@Book", txtBook.Text);
+                cmd.Parameters.AddWithValue("@First_Name", txtFirst.Text);
+                cmd.Parameters.AddWithValue("@Last_Name", txtLast.Text);
+                cmd.Parameters.AddWithValue("@Publisher", txtPublisher.Text);
+                cmd.Parameters.AddWithValue("@Genre", comboGenre.SelectedItem);
+                cmd.Parameters.AddWithValue("@Release_Year", double.Parse(txtRelease.Text));
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                MessageBox.Show("Successfully Added");
+
+                Clear();
+            }
+        }
+
+        //Clears data from the textboxes
+        public void Clear()
+        {
+            txtBook.Text = "";
+            txtFirst.Text = "";
+            txtLast.Text = "";
+            txtPublisher.Text = "";
+            txtRelease.Text = "";
+            comboGenre.Text = "Please Select An Item";
+        }
+
+        //Deletes data from the database
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE Books WHERE Book=@Book", con);
+                cmd.Parameters.AddWithValue("@Book", txtBook.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();    
+                MessageBox.Show("Successfully Deleted");
+            }
+        }
+
+        //Updates data in the database
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString)) 
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Books set First_Name=@First_Name, Last_Name=@Last_Name, Publisher = @Publisher, Genre = @Genre, Release_Year = @Release_Year WHERE Book = @Book", con);
+                cmd.Parameters.AddWithValue("@Book", txtBook.Text);
+                cmd.Parameters.AddWithValue("@First_Name", txtFirst.Text);
+                cmd.Parameters.AddWithValue("@Last_Name", txtLast.Text);
+                cmd.Parameters.AddWithValue("@Publisher", txtPublisher.Text);
+                cmd.Parameters.AddWithValue("@Genre", comboGenre.SelectedItem);
+                cmd.Parameters.AddWithValue("@Release_Year", double.Parse(txtRelease.Text));
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                MessageBox.Show("Successfully Updated");
+
+                Clear();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBook.Text = "";
+            txtFirst.Text = "";
+            txtLast.Text = "";
+            txtPublisher.Text = "";
+            txtRelease.Text = "";
+            comboGenre.Text = "Please Select An Item";
+        }
+    }
+}
